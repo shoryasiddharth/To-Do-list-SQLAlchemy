cef8228980a2a966963274b5b1e05abbe3d899f3
siddharth.shorya <siddharth.shorya@mu-sigma.com>
2022-09-23 02:02:39 +0530

    WIP for adding try, except and finnaly in the route handler code

--- app.py
+++ app.py
@@ -1,18 +1,39 @@
 
-from flask import Flask, render_template
+from doctest import debug_script
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/mydb'
+db = SQLAlchemy(app)
+
+
+class ToDo(db.Model):
+    __tablename__ = 'todo'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<ToDo id = {self.id} desc = {self.description}'
+
+
+db.create_all()
+
+
+@app.route('/todo/create', methods=['POST'])
+def create_todo():
+    description = request.get_json()['description']
+    todo = ToDo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return jsonify({
+        'description': todo.description
+    })
 
 
 @app.route('/')
 def index():
-    return render_template('./index.html', data=[{
-        'description': 'Todo 1'
-    }, {
-        'description': 'Todo 2'
-    }, {
-        'description': 'Todo 3'
-    }])
+    return render_template('./index.html', data=ToDo.query.all())
 
 
 # always include this at the bottom of your code (port 3000 is only necessary in workspaces)

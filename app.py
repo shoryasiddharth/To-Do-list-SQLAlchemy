2bbf1b4da3450871328bb528a823c0c575b06626
siddharth.shorya <siddharth.shorya@mu-sigma.com>
2022-09-23 20:00:01 +0530

    added the update checked in the DB

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 
+from crypt import methods
+from email.policy import default
 import sys
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
@@ -12,6 +14,7 @@ class ToDo(db.Model):
     __tablename__ = 'todo'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, default=False, nullable=False)
 
     def __repr__(self):
         return f'<ToDo id = {self.id} desc = {self.description}'
@@ -40,13 +43,26 @@ def create_todo():
         abort(500)
     return jsonify(body)
 
-
+@app.route('/todo/<item_id>/update-checked', methods=['POST'])
+def updateCheckStatus(item_id):
+    body={}
+    try:
+        checkStatus = request.get_json()['completed']
+        todo = ToDo.query.get(item_id)
+        todo.completed = checkStatus
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally: 
+        db.session.close()
+    return redirect(url_for('index'))
 @app.route('/')
 def index():
-    return render_template('./index.html', data=ToDo.query.all())
+    return render_template('./index.html', data=ToDo.query.order_by("id").all())
 
 
 # always include this at the bottom of your code (port 3000 is only necessary in workspaces)
 
 if __name__ == '__main__':
+    # app.run(debug=True)
     app.run(host="0.0.0.0", port=3000)
